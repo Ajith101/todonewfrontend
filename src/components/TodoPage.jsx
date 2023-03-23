a46eaade0666772ef0903111a2cdf6bea1f7ea95
Ajith101 <ajithpktd@gmail.com>
2023-03-23 10:48:36 +0530

    size change

--- src/components/TodoPage.jsx
+++ src/components/TodoPage.jsx
@@ -5,7 +5,7 @@ import TodoLists from "./todoPage/TodoLists";
 const TodoPage = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="bg-yellow-200 w-[95%] md:w-[60%] p-3 my-4 rounded-md md:my-10">
+      <div className="bg-yellow-200 w-[90%] md:w-[60%] p-3 my-4 rounded-md md:my-10">
         <h1 className="text-2xl mb-4 font-bold font-font-2">
           Add Your Favorite notes Here
         </h1>
